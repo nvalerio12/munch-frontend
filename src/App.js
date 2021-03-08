d2506fc8974e4273b69004159725531939f2eb2b
nvalerio12 <nelson.j.valerio@gmail.com>
2021-03-08 16:46:48 -0500

    made navbar and side bar data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import './App.css';
+import NavbarMunch from './components/NavbarMunch';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home'
+
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+    <Router>
+    <NavbarMunch />
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/SidebarData.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as AiIcons from 'react-icons/ai';
+
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+
+  {
+    title: 'About Us',
+    path: '/',
+    icon: <AiIcons.AiFillInfoCircle />,
+    cName: 'nav-text'
+  },
+  
+  
+];
