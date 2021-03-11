d3b28f40fd36596e8aa79acb191b42a63ec6298d
Andrew8bit <Andrewbith@gmail.com>
2021-03-10 17:33:56 -0800

    added index to components/feed - exported feed - destructred in app.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,5 @@ export { default as Login } from './Login';
 export { default as Profile } from './Profile';
 export { default as Signup } from './Signup';
 export { default as Restaurant } from './Restaurant';
+export { default as Feed } from './Feed';
 
