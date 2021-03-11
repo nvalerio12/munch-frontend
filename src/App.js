ea13456bbcb904a52fb16a2461fc7daf4d169852
Andrew8bit <Andrewbith@gmail.com>
2021-03-10 16:30:11 -0800

    restructured sidebar using guide from css-tricks

--- src/App.js
+++ src/App.js
@@ -1,61 +1,90 @@
 import React from "react";
-import "./App.css";
+import Navbar from "./components/Navbar";
+import SideNav from "./components/SideNav";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+
 import NavbarMunch from "./components/partials/navbars/NavbarMunch";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import About from "./components/About";
 import Profile from "./components/Profile";
-import { useEffect, useState } from 'react';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 
-const PrivateRoute = ({ component: Component, ...rest}) => {
-  let token = localStorage.getItem('jwtToken');
-  console.log('===> Hitting a Private Route');
-  return <Route {...rest} render={(props) => {
-    return token ? <Component {...rest} {...props} /> : <Redirect to="/login"/>
-  }} />
-}
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  let token = localStorage.getItem("jwtToken");
+  console.log("===> Hitting a Private Route");
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return token ? (
+          <Component {...rest} {...props} />
+        ) : (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+  );
+};
 
 function App() {
-    // Set state values
-    const [currentUser, setCurrentUser] = useState('');
-    const [isAuthenticated, setIsAuthenticated] = useState(true);
-   
-    useEffect(() => {
-      let token;
-  
-      if (!localStorage.getItem('jwtToken')) {
-        setIsAuthenticated(false);
-        console.log('====> Authenticated is now FALSE');
-      } else {
-        token = jwt_decode(localStorage.getItem('jwtToken'));
-        setAuthToken(localStorage.getItem('jwtToken'));
-        setCurrentUser(token);
-      }
-    }, []);
-  
-    const nowCurrentUser = (userData) => {
-      console.log('===> nowCurrent is here.');
-      setCurrentUser(userData);
-      setIsAuthenticated(true);
+  // Set state values
+  const [currentUser, setCurrentUser] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    let token;
+
+    if (!localStorage.getItem("jwtToken")) {
+      setIsAuthenticated(false);
+      console.log("====> Authenticated is now FALSE");
+    } else {
+      token = jwt_decode(localStorage.getItem("jwtToken"));
+      setAuthToken(localStorage.getItem("jwtToken"));
+      setCurrentUser(token);
     }
-  
-    const handleLogout = () => {
-      if (localStorage.getItem('jwtToken')) {
-        // remove token for localStorage
-        localStorage.removeItem('jwtToken');
-        setCurrentUser(null);
-        setIsAuthenticated(false);
-      }
+  }, []);
+
+  const nowCurrentUser = (userData) => {
+    console.log("===> nowCurrent is here.");
+    setCurrentUser(userData);
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    if (localStorage.getItem("jwtToken")) {
+      // remove token for localStorage
+      localStorage.removeItem("jwtToken");
+      setCurrentUser(null);
+      setIsAuthenticated(false);
     }
+  };
 
   return (
     <>
       <Router>
-        <NavbarMunch currentUser={currentUser} isAuthenticated={isAuthenticated} />
+        <>
+        <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <div>
+          <Navbar open={open} setOpen={setOpen} />
+          <SideNav open={open} setOpen={setOpen} />
+        </div>
+      </ThemeProvider>
+        </>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/signup" exact component={Signup} />
--- src/components/partials/navbars/NavbarMunch.js
+++ src/components/partials/navbars/NavbarMunch.js
@@ -7,11 +7,9 @@ import './NavbarMunch.css';
 import { IconContext } from 'react-icons';
 
 function NavbarMunch(props) {
-  const [currentUser, setCurrentUser] = useState(props.currentUser)
-  const [isAuthenticated, setIsAuthenticated] = useState(props.isAuthenticated)
   const [sidebar, setSidebar] = useState(false);
 
-  const showSidebar = () => setSidebar(!sidebar); // setting state to opposite of current sidebar state
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <>
@@ -21,12 +19,13 @@ function NavbarMunch(props) {
         <FaIcons.FaBars onClick={showSidebar} />
       </Link>
     </div>
-    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-      <ul className='nav-menu-items' onClick={showSidebar}>
+    <div class="nav-icon">
+      <div></div>
+    </div>
+    <nav className={sidebar ? 'nav-menu active tabindex=`1`' : 'nav-menu'}>
+      <ul className='nav-menu-items' >
         <li className='navbar-toggle'>
-          <Link to='#' className='menu-bars'>
-            <AiIcons.AiOutlineClose />
-          </Link>
+          
         </li>
         {SidebarData.map((item, index) => {
           return (
